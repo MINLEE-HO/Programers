5f5a43335fbf17935eae7281830bbc6af625fd50
MINLEE-HO <qqq5656@naver.com>
2025-04-22 00:11:57 +0900

    일일도전과제_Day1

---
+++ Day_1/D1_05.java
@@ -0,0 +1,14 @@
+package Day_1;
+
+/*
+문제 설명
+다음과 같이 출력하도록 코드를 작성해 주세요.
+
+출력 예시
+!@#$%^&*(\'"<>?:;
+ */
+public class D1_05 {
+    public static void main(String[] argv){
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+    }
+}
